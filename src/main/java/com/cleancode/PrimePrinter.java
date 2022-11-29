3099d717233a6d8c9cff61cee841e9c47201e441
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2022-11-29 17:49:35 +0900

    getting~~

--- src/main/java/com/cleancode/PrimePrinter.java
+++ src/main/java/com/cleancode/PrimePrinter.java
@@ -1,30 +1,48 @@
 package com.cleancode;
 
 public class PrimePrinter {
+    static final int M = 1000;
+    static final int RR = 50;
+    static final int CC = 4;
+    static final int ORDMAX = 30;
+    static int P[] = new int[M+1];
+    static int J = 1;
+    static int K = 1;
+    static int ORD = 2;
+    static int SQUARE = 9;
+    static boolean JPRIME = true;
+    static int MULT[] = new int[ORDMAX+1];
+    static int N = 0;
+    static int ROWOFFSET = 1;
+    static int PAGENUMBER = 1;
+    static int PAGEOFFSET = 1;
+
     public static void main(String[] args) {
-        final int M = 1000;
-        final int RR = 50;
-        final int CC = 4;
-        final int ORDMAX = 30;
-        int P[] = new int[M+1];
-        int PAGENUMBER;
-        int PAGEOFFSET;
-        int ROWOFFSET;
-        int C;
-        int J;
-        int K;
-        boolean JPRIME;
-        int ORD;
-        int SQUARE;
-        int N=0;
-        int MULT[] = new int[ORDMAX+1];
 
-        J=1;
-        K=1;
         P[1] = 2;
-        ORD = 2;
-        SQUARE = 9;
 
+        whileKtoM();
+        PAGENUMBER = 1;
+        while (PAGEOFFSET <= M) {
+            printPage();
+            PAGENUMBER++;
+            PAGEOFFSET += RR*CC;
+
+        }
+    }
+
+    private static void printPage() {
+        System.out.print("The First " + Integer.toString(M) + " Prime Numbers === Page " + Integer.toString(PAGENUMBER) + "\n");
+        for (ROWOFFSET= PAGEOFFSET; ROWOFFSET <= PAGEOFFSET +RR-1; ROWOFFSET++) {
+            for (int C = 0; C <= CC - 1; C++)
+                if (ROWOFFSET + C * RR <= M)
+                    System.out.printf("%10d", P[ROWOFFSET + C * RR]);
+            System.out.println();
+        }
+        System.out.println("\f");
+    }
+
+    private static void whileKtoM() {
         while (K < M) {
             do {
                 J += 2;
@@ -34,7 +52,6 @@ public class PrimePrinter {
                     MULT[ORD-1]=J;
                 }
                 N=2;
-                JPRIME=true;
                 while (N < ORD && JPRIME) {
                     while (MULT[N]<J)
                         MULT[N] += P[N] + P[N];
@@ -45,25 +62,6 @@ public class PrimePrinter {
             } while (!JPRIME);
             K++;
             P[K]=J;
-        }
-        PAGENUMBER = 1;
-        PAGEOFFSET = 1;
-        while (PAGEOFFSET <= M) {
-            System.out.print("The First ");
-            System.out.print(Integer.toString(M));
-            System.out.print(" Prime Numbers === Page ");
-            System.out.print(Integer.toString(PAGENUMBER));
-            System.out.println("\n");
-            for (ROWOFFSET=PAGEOFFSET; ROWOFFSET <= PAGEOFFSET+RR-1; ROWOFFSET++) {
-                for (C = 0; C <= CC - 1; C++)
-                    if (ROWOFFSET + C * RR <= M)
-                        System.out.printf("%10d", P[ROWOFFSET + C * RR]);
-                System.out.println();
-            }
-            System.out.println("\f");
-            PAGENUMBER++;
-            PAGEOFFSET += RR*CC;
-
         }
     }
 }
